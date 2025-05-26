5abae63f047ded5d05874f914e1fc355e6bf2653
Naomi Rivkin <nomi420@gmail.com>
2025-05-26 14:04:33 +0300

    add urls

---
+++ urls.js
@@ -0,0 +1,39 @@
+export const baseUrl = "https://www2.hm.com/hw_il";
+
+export const urls = [
+  {
+    url: "/kids/last-chance/2-8y.html",
+    label: "Kids 2-8Y",
+    fileName: "kids_2_8y.json",
+  },
+  {
+    url: "/kids/last-chance/9-14y.html",
+    label: "Kids 9-14Y",
+    fileName: "kids_9_14y.json",
+  },
+  {
+    url: "/home/last-chance/view-all.html",
+    label: "Home",
+    fileName: "home.json",
+  },
+  {
+    url: "/baby/last-chance/newborn.html",
+    label: "Newborn",
+    fileName: "newborn.json",
+  },
+  {
+    url: "/baby/last-chance/baby.html",
+    label: "Baby",
+    fileName: "baby.json",
+  },
+  {
+    url: "/ladies/last-chance/view-all.html",
+    label: "Women",
+    fileName: "women.json",
+  },
+  {
+    url: "/men/last-chance/view-all.html",
+    label: "Men",
+    fileName: "men.json",
+  },
+];
