6cd6f2dabe565020c78bdcff20eff4120ed59a41
Naomi Rivkin <nomi420@gmail.com>
2025-05-29 23:01:05 +0300

    Refactor logger configuration to remove dotenv dependency

--- logger.js
+++ logger.js
@@ -1,10 +1,9 @@
 import pino from "pino";
-import dotenv from "dotenv";
 
-dotenv.config();
+const level = process.env.PINO_LOG_LEVEL ?? "info";
 
 const logger = pino({
-  level: process.env.PINO_LOG_LEVEL || "info",
+  level,
   timestamp: () => `,"timestamp":"${new Date(Date.now()).toISOString()}"`,
 });
 
